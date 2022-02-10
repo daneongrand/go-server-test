3050d9cad43099e477a3c42f66cbc142ab58ba0e
daneongrand <danektolstov@gmail.com>
2022-02-10 23:37:09 +0400

    Add styles and .onclose function

---
+++ server.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"github.com/gorilla/websocket"
+)
+
+var clients map[*websocket.Conn]bool
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func main() {
+	clients = make(map[*websocket.Conn]bool)
+	http.HandleFunc("/", swHandler)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func swHandler (w http.ResponseWriter, r *http.Request) {
+	connection, _ := upgrader.Upgrade(w, r, nil)
+	defer connection.Close()
+	clients[connection] = true
+
+	for {
+		_, message, err := connection.ReadMessage()
+
+		if err != nil {
+			log.Fatal(err)
+			break
+		}
+
+		go writeMessage(message)
+		go loggerMessage(message)
+	}
+
+}
+
+func loggerMessage(message []byte)  {
+	log.Println(string(message))
+}
+
+func writeMessage (message []byte) {
+	for connection := range clients {
+		connection.WriteMessage(websocket.TextMessage, message)
+	}
+}
